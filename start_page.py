49c02710d2a1b547fb30451079ddc3127b6ef8e5
DotSnuk <dotsnuk@outlook.com>
2020-02-25 19:30:44 +0100

    Changed names of files, made the first page visitable

--- start_page.py
+++ start_page.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 login_app = Flask(__name__)
 
 @login_app.route('/')
-def login_page() -> 'html':
+def start_page() -> 'html':
     return render_template('home.html', the_title='Welcome to my simple login tester')
 
+@login_app.route('/login')
+def login_page() -> 'html':
+    return render_template('login.html', the_title='Welcome!')
 
 
 login_app.run(debug=True)
